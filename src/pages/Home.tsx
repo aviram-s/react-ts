852cc95fb2f3b8fdb995fb0faafa34ebb9ee248e
aviram-s <aviram.shany.90@gmail.com>
2022-03-23 17:40:31 +0200

    body ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,32 +3,128 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <ul className="catalog">
-        <li className="product">
-          <h2>asdsadsadadasdsadsadasd</h2>
-          <p>asdsadadsad</p>
-        </li>
-        <li className="product">
-          <h2>asdsadsadadasdsadsadasd</h2>
-          <p>asdsadadsad</p>
-        </li>
-        <li className="product">
-          <h2>asdsadsadadasdsadsadasd</h2>
-          <p>asdsadadsad</p>
-        </li>
-        <li className="product">
-          <h2>asdsadsadadasdsadsadasd</h2>
-          <p>asdsadadsad</p>
-        </li>
-        <li className="product">
-          <h2>asdsadsadadasdsadsadasd</h2>
-          <p>asdsadadsad</p>
-        </li>
-        <li className="product">
-          <h2>asdsadsadadasdsadsadasd</h2>
-          <p>asdsadadsad</p>
-        </li>
-      </ul>
+      <div className="catalog">
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+        <div className="product">
+          <img src="https://images.squarespace-cdn.com/content/v1/576bf6f9197aeaf55ed0447d/1647921430677-UAP8BAAZLEN2S40S7X2Z/image-asset.png"></img>
+          <div className="productBody">
+            <a>
+              OMEN by HP Photon Wireless Gaming Mouse with Qi Wireless Charging
+            </a>
+            <p>
+              From $127 to <b>$10</b>
+            </p>
+            <p style={{ opacity: "0.7", fontSize: "0.8rem" }}>Mar 21, 2022</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
